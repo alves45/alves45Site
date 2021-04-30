01f4e52050eda3064c3b2fe32dfa3ffb09a47667
alves45 <alvesdelima.lucas45@gmail.com>
2021-04-29 22:43:34 -0300

    deu certo

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+  const http = require('http')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  http.createServer((req,res)=>{
+      res.write('Deu ceretttooo Lucas Alves')
+      res.end();
+  }).listen(5000)
