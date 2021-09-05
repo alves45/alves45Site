a8e9737065902942405ef9aa645254dc46b2c077
alves45 <alvesdelima.lucas45@gmail.com>
2021-09-05 01:33:31 -0300

    agora vai

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const http = require('http');
-const fs = require('fs');
-const PORT = process.env.PORT || 5000;
-const page = fs.readFileSync('index.html', 'utf8');
+const http = require('http')
+const fs = require('fs')
+const PORT = process.env.PORT || 3000
+const page = fs.readFileSync('index.html', 'utf8')
 
-http.createServer((req, res) => {
+http
+	.createServer((req, res) => {
 		if (req.headers['x-forwarded-proto'] == 'https') {
-			res.statusCode = 200;
-			res.setHeader('Content-Type', 'html');
-			res.end(page);
+			res.statusCode = 200
+			res.setHeader('Content-Type', 'html')
+			res.end(page)
 		} else {
-			res.writeHead(301, { Location: 'https://' + req.headers['headerName'] });
-			res.end();
+			res.writeHead(302, { Location: 'https://'+ req.headers.host ,})
+			res.end()
 		}
-		console.log(req.headers.host);
 	})
-	.listen(PORT);
+	.listen(PORT)
