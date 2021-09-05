307cec36e42d7aa607e6d0c67c89e54edf538157
alves45 <alvesdelima.lucas45@gmail.com>
2021-09-05 01:21:09 -0300

    https  init

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const http = require('http')
-const GenPages = require('./WebPages/GenPages.js').GenPages
-const PORT = process.env.PORT || 5000
+const http = require('http');
+const fs = require('fs');
+const PORT = process.env.PORT || 5000;
+const page = fs.readFileSync('index.html', 'utf8');
 
-const pages = {
-  login: GenPages('Login')
-}
-
-http.createServer((req,res)=>{
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'html')
-  res.end(GenPages('Login'))//pages.login)
-}).listen(PORT)
-console.log('OK')
+http.createServer((req, res) => {
+		if (req.headers['x-forwarded-proto'] == 'https') {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'html');
+			res.end(page);
+		} else {
+			res.writeHead(301, { Location: 'https://' + req.headers.Host });
+		}
+		console.log(req.headers.host);
+	})
+	.listen(PORT);
