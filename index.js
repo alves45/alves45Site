96e7956ebaa0ee9a17d12f0fb16cfce95bd392e2
alves45 <alvesdelima.lucas45@gmail.com>
2021-09-06 00:23:58 -0300

    Feito correção de nomes de variáveis

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const http = require('http')
-const fs = require('fs')
-const AppUrl = 'https://alves45.herokuapp.com/'
+import { createServer } from "http"
+import { readFileSync } from "fs"
+const AppUrl = "https://alves45.herokuapp.com/"
 const PORT = process.env.PORT || 3000
-const page = fs.readFileSync('index.html', 'utf8')
+const pageLogin = readFileSync("login.html", "utf8")
 
-http
-	.createServer((req, res) => {
-		if (req.headers['x-forwarded-proto'] == 'https') {
-			res.statusCode = 200
-			res.setHeader('Content-Type', 'html')
-			res.end(page)
-		} else {
-			res.writeHead(302, { Location: AppUrl})
-			res.end()
-		}
-	})
-	.listen(PORT)
+createServer((req, res) => {
+	res.statusCode = 200
+	if (req.headers["x-forwarded-proto"] == "https") {
+		let response = ''
+		req.addListener('data', (chunk) => {
+			response = chunk
+		})
+		req.addListener('end', (a) => {
+			if (response) {
+				res.setHeader('Consent-Type', 'application/json')
+				res.end(response)
+			} else {
+				res.setHeader('Content-Type', 'text/html')
+				res.end(pageLogin)
+			}
+		})
+	} else {
+		res.writeHead(302, { 'Location': AppUrl })
+		res.end()
+	}
+}).listen(PORT)
+
+
