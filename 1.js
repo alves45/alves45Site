0c42d9d240d27d6e103e05d095821e975411ebce
alves45 <alvesdelima.lucas45@gmail.com>
2022-01-26 08:01:05 -0300

    change repository CRLF to LF

---
+++ 1.js
@@ -0,0 +1,26 @@
+// import babel from "@babel/core";
+// console.log(
+//   babel.transformSync("()=>{console.log('123')}", {
+//     plugins: ["@babel/plugin-transform-arrow-functions"],
+//   })
+// );
+
+// import { promises as fs } from "fs";
+// import path from "path";
+// async function crLfToLf(dirPath) {
+//   var list = await fs.readdir(dirPath);
+//   list.map(async (pathElement) => {
+//     pathElement = path.resolve(dirPath, pathElement);
+//     var that = await fs.lstat(pathElement);
+//     if (that.isFile()) {
+//       console.log(pathElement);
+//       fs.writeFile(
+//         pathElement,
+//         (await fs.readFile(pathElement, "utf8")).replace(/(\n|\r|\r\n)/g, "\n")
+//       );
+//     } else {
+//       crLfToLf(pathElement);
+//     }
+//   });
+// }
+// crLfToLf("./");
--- Pages/tools/classPages.js
+++ Pages/tools/classPages.js
@@ -1,97 +1,194 @@
 import fs from "fs";
+
 import zlib from "zlib";
+
 import { JSDOM } from "jsdom";
+
 import { css, cache } from "@emotion/css";
+
 import { consts, _style } from "./css.js";
+
 const whitePage = fs.readFileSync("./whiteWindow.html", "utf8");
+
 export default class page {
+
   constructor() {
+
     /**@type {Window} */
+
     this.window = new JSDOM(whitePage).window;
+
     this.HTML = this.compress = "";
+
     this.style = new _style(cache);
+
   }
+
   render() {
+
     let document = this.window.document;
+
     if (process.env.NODE_ENV !== "production") {
+
       let devReloadScript = document.createElement("script");
+
       document.head.appendChild(devReloadScript);
+
       devReloadScript.innerHTML =
+
         "(" +
+
         (() => {
+
           console.log("tá dando boa");
+
           let toggle = false;
+
           let controller = new AbortController();
+
           let signal = controller.signal;
+
           setInterval(() => {
+
             var timeout = setTimeout(() => {
+
               controller.abort();
+
             }, 200);
+
             fetch("", { signal })
+
               .then(() => {
+
                 clearInterval(timeout);
+
                 if (toggle) {
+
                   location.reload();
+
                 } else {
+
                 }
+
               })
+
               .catch(() => {
+
                 clearInterval(timeout);
+
                 controller = new AbortController();
+
                 signal = controller.signal;
+
                 toggle = true;
+
               });
+
           }, 500);
+
         }).toString() +
+
         ")()";
+
     }
+
     const styleDOM = document.createElement("style");
+
     this.style.addG(css`
+
       * {
+
         box-sizing: border-box;
+
         margin: 0;
+
         padding: 0;
+
         font-family: Arial, Helvetica, sans-serif;
+
         transition: all ease-in-out 200ms;
+
       }
+
       @font-face {
+
         font-family: "icons";
+
         font-style: normal;
+
         font-weight: 400;
+
         src: url(https://fonts.gstatic.com/s/materialicons/v117/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2)
+
           format("woff2");
+
       }
+
       .icons {
+
         font-family: "icons";
+
         font-size: 1rem;
+
       }
+
       :root {
+
         font-size: 6vw;
+
       }
+
       @media (max-aspect-ratio: 1/1) {
+
         :root {
+
           font-size: 6vw;
+
         }
+
       }
+
       @media (pointer: fine) {
+
         :root {
+
           font-size: min(3vh, 3vw);
+
         }
+
       }
+
       body {
+
         background-color: ${consts.gray900};
+
       }
+
     `);
+
     styleDOM.innerHTML = this.style.buildStyles();
+
     document.head.appendChild(styleDOM);
+
     if (process.env.NODE_ENV !== "production") {
+
       this.HTML = document.documentElement.innerHTML;
+
       this.compress = "utf-8";
+
     } else {
+
       this.HTML = zlib.brotliCompressSync(
+
         new Buffer.from(document.documentElement.innerHTML)
+
       );
+
       this.compress = "br";
+
     }
+
   }
+
 }
+
